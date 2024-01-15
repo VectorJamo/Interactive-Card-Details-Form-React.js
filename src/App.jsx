63d78378ad00790200ad31525690a26afc3594e9
VectorJamo <nsura552@gmail.com>
2024-01-14 21:15:06 -0700

    Added features

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css'
 
+import cardFrontImage from './assets/images/bg-card-front.png'
+import cardBackImage from './assets/images/bg-card-back.png'
+import cardLogo from './assets/images/card-logo.svg'
+import completeIcon from './assets/images/icon-complete.svg'
+
 export default function App() {
 
   /*
@@ -31,5 +36,40 @@ export default function App() {
     Continue
   */ 
 
-  return "Hello";
+
+  return (
+    <div className="site-container">
+      <div className="site-left">
+        <div className="card-front-image">
+          <img src={cardFrontImage} alt="Card's front image" />
+          <div className="card-logo-image">
+            <img src={cardLogo} alt="Card's logo image" />
+          </div>
+          <p className="card-number">XXXX XXXX XXXX XXXX</p>
+          <div className="card-details">
+            <p className="card-holder-name">John Doe</p>
+            <p className="card-expiry-date">05/25</p>
+          </div>
+        </div>
+        <div className="card-back-image">
+          <img src={cardBackImage} alt="Card's back image" />
+          <p className="card-cvv">XXX</p>
+        </div>
+      </div>
+      <div className="site-right">
+        <form action="#">
+          <div className="form-piece">
+            <label htmlFor="name">CARDHOLDER NAME</label>
+            <br />
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className="form-piece">
+            <label htmlFor="number">CARD NUMBER</label>
+            <br />
+            <input type="text" id="number" name="number" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
